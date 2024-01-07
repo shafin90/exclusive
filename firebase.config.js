412d027401734c69151d474617b98d5651db7f8e
shafin90 <mashrafiahnam1@gmail.com>
2024-01-07 04:59:35 -0800

    push

---
+++ firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxr1LCCCUw-LnO5XlCWsu2jKIMfP0eIXM",
+  authDomain: "exclusive-14fdd.firebaseapp.com",
+  projectId: "exclusive-14fdd",
+  storageBucket: "exclusive-14fdd.appspot.com",
+  messagingSenderId: "554820492938",
+  appId: "1:554820492938:web:5a0d7b34308790de9ed773"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/app/registerPage/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div className=' mx-auto'>
+            
+        </div>
+    );
+};
+
+export default page;
